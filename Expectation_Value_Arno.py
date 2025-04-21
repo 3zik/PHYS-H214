c44d46d26edfcd178b29c54e2d819a2681932720
3zik <ethanzfurman@gmail.com>
2025-04-21 19:48:03 -0400

    updated w/ propoer np command

--- Expectation_Value_Arno.py
+++ Expectation_Value_Arno.py
@@ -9,22 +9,22 @@ def x_expectation(r, wavefns):
         efunc = wavefns[:, i]
         integrand1 = r * efunc**2
         integrand2 = (r**2) * efunc**2
-        x1 = np.trapezoid(integrand1, r)
-        x2 = np.trapezoid(integrand2, r)
+        x1 = np.trapz(integrand1, r) # can be trapezoid or trapz depending on numpy version
+        x2 = np.trapz(integrand2, r) # can be trapezoid or trapz depending on numpy version
         deltax = np.sqrt(x2 - x1**2)
         delxstorage.append(deltax)
     print(delxstorage)
 
 def p_expectation(r, wavefns):
     #This computes the standard deviation Δp = sqrt(<p²> - <p>²) for each wavefunction
-    h = 1.054571817 * 10**-34
+    h_bar = 1.054571817 * 10**-34
     for i in range(wavefns.shape[1]):
         efunc = wavefns[:, i]
-        integrand1 = np.gradient(h*efunc**2)
-        integrand2 = np.gradient(np.gradient(-h**2*efunc**2))
-        p1 = np.trapezoid(integrand1,r)
-        p2 = np.transpose(integrand2,r)
-        deltap = np.sqrat(p2 + p1**2)
+        integrand1 = np.gradient(h_bar*efunc**2)
+        integrand2 = np.gradient(np.gradient(-h_bar**2*efunc**2))
+        p1 = np.trapz(integrand1,r) # can be trapezoid or trapz depending on numpy version
+        p2 = np.trapz(integrand2,r) # can be trapezoid or trapz depending on numpy version
+        deltap = np.sqrt(p2 + p1**2)
         delpstorage.append(deltap)
     print(delpstorage)
 
